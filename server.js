7a720b8901ad66f3a7055be9fba4bbd18c507648
unknown <williamgilbertgo@gmail.com>
2018-08-19 12:07:56 -0700

    Change port to 5000

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
