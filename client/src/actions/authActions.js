adcbb3d015a9f8b0b876cbb4097ee0dd434eb920
unknown <williamgilbertgo@gmail.com>
2018-08-22 01:01:30 -0700

    Create new action creators for the logged in and current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,7 +1,9 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { setAuthToken } from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
-// Register User
+// Register User - an Action Creator
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
@@ -12,4 +14,36 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login - Get user token - an Action Creator
+export const loginuser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+      // Set token to LS
+      localStorage.setItem("jwtToken", token);
+      // Set token to auth header
+      setAuthToken(token);
+      // Decode the token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decode));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set logged in user - an Action Creator
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
