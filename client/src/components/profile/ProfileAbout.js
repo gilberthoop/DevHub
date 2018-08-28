ae424967589372e1685b4fcce70adf10f2545864
unknown <williamgilbertgo@gmail.com>
2018-08-27 20:58:24 -0700

    Create the components for individual profile

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ProfileAbout extends Component {
+  render() {
+    return <div>PROFILE ABOUT</div>;
+  }
+}
+
+export default ProfileAbout;
---
+++ client/src/components/profile/ProfileCreds.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ProfileCreds extends Component {
+  render() {
+    return <div>CREDS</div>;
+  }
+}
+
+export default ProfileCreds;
