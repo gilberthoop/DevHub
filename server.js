e3966525d0b8a27198ce9e234806099a9575da4d
unknown <williamgilbertgo@gmail.com>
2018-08-30 01:26:28 -0700

    Prepared for deploy

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,6 +31,16 @@ require("./config/passport.js")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set a static folder
+  app.use(express.static("client/build"));
+  // Default route
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
