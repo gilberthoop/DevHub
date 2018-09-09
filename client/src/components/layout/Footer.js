96ab2c1e5b35b14d8ac19a7749f95dcb81fa4806
unknown <williamgilbertgo@gmail.com>
2018-09-08 22:35:03 -0700

    -

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default () => {
   return (
     <footer className="bg-dark text-white mt-5 p-3 text-center">
-      Copyright &copy; {new Date().getFullYear()} DevHub{" "}
+      Copyright &copy; {new Date().getFullYear()} William Go{" "}
     </footer>
   );
 };
